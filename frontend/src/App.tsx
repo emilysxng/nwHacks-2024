582a414f84740c83af2f52c8d1217ceb81ef63b9
tczg <tczg2004@gmail.com>
2024-01-20 12:56:04 -0800

    base chakra ui settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <Box bg='tomato' w='100%' p={4} color='white'>
+      <Box bg='tomato' w='100%' p={4} color='white' >
         This is the Box
       </Box>
     </>
