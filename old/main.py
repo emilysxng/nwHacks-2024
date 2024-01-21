bd64670770f3b07f8fe7f2e476d2dd6c03979b4c
emilysxng <song.emily2004@gmail.com>
2024-01-20 22:42:58 -0800

    h

--- old/main.py
+++ old/main.py
@@ -1,7 +1,18 @@
 from study_session import startStudySession
+from test import measure_decibels
+from test import forever
+import threading
 
 def main():
-    startStudySession()
+    measure_decibels()
 
 if __name__ == "__main__":
+    """     # Create a thread for the parallel function
+    parallel_thread = threading.Thread(target=forever)
+
+    # Start the parallel thread
+    parallel_thread.start()
+
+    # Run the main function in the main thread """
+    
     main()
--- old/test.py
+++ old/test.py
@@ -1,102 +1,62 @@
-import soundmeter
+import pyaudio
 from statistics import mean
+import wave
+import numpy as np
+import wavio
+import math
 
 def measure_decibels():
-    meter = soundmeter.Meter()
-    meter.start()
+    chunk = 1024
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100
+    filename = "output.wav"
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-    print("Measuring decibels. Press Ctrl+C to stop.")
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
 
     try:
         while True:
-            decibels = []
-            decibels.append(meter.get_db())
-            print(f"Current decibel level: {decibels:.2f} dB")
-    except KeyboardInterrupt:
-        meter.stop()
-        avgDecibels = mean(decibels)
-        print(f"\nAverage decibel level: {avgDecibels:.2f} dB")
+            data = stream.read(chunk)
+            frames.append(data)
+    except:
+        KeyboardInterrupt
 
+    #where they should join``
+    ################################################
 
-import cv2
-import time
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
 
-def startStudySession():
-    # the cascadeClassifier class constructor takes a trained classifier file as its argument
-    cascadeClassifierFace = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    cascadeClassifierEyes = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-    videoFeed = cv2.VideoCapture(0)
-    startTime = time.time()
-    studyFrameCount = 0
-    lookDownFrameCount = 0
-    AFKFrameCount = 0
-    totalFrameCount = 0
-    isAFK = False
-    AFKCount = 0
+    print('Finished recording')
 
-    while True:
-        isSuccess, videoFrame = videoFeed.read() # from handle_frame
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
-        if isSuccess is False:
-            break
+    # Load the .wav file
+    wav_data = wavio.read(filename)
 
-        totalFrameCount += 1
-        face = faceDetection(videoFrame, cascadeClassifierFace)
-        eyes = eyeDetection(videoFrame, cascadeClassifierEyes)
-        cv2.imshow("Active Study Session", videoFrame) 
+    # Extract audio data from the wavio.Wav object
+    audio_data = wav_data.data.T[0]  # Extract the first channel
 
-        if len(face) > 0 and len(eyes) >= 2:
-            print("Studying")
-            studyFrameCount += 1
-            isAFK = False
-        elif len(face) > 0 and len(eyes) < 2: 
-            print("Looking away")
-            lookDownFrameCount += 1
-            isAFK = False
-        else:
-            print("AFK")
-            AFKFrameCount += 1
+    chunks = np.array_split(audio_data, audio_data.size/(44100/2))
+    dbs = [20*math.log10(np.abs(math.sqrt(mean(chunk**2))) ) for chunk in chunks] #half second chunks
+    dbs = mean(dbs)
+    print(dbs)
 
-            if (isAFK is False):
-                isAFK = True
-                AFKCount += 1
-            else:
-                continue
-
-        # quit if q is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            studyPercent = round(studyFrameCount / totalFrameCount * 100, 2)
-            lookDownPercent = round(lookDownFrameCount / totalFrameCount * 100, 2)
-            AFKPercent = round(AFKFrameCount / totalFrameCount * 100, 2)
-            elapsedTime = round(time.time() - startTime, 2)
-
-            print("Total study session length:", elapsedTime, "seconds")
-            print("Percent of time spent working:", studyPercent, "%")
-            print("Percent of time spent on your phone:", lookDownPercent, "%")
-            print("Percent of time spent AFK:", AFKPercent, "%")
-            print("Approximate number of times you left your desk:", AFKCount)
-            break
-        
-    videoFeed.release()
-    cv2.destroyAllWindows()
-
-
-def faceDetection(videoFrame, cascadeClassifierFace):
-    greyVideoFrame = cv2.cvtColor(videoFrame, cv2.COLOR_BGR2GRAY)
-
-    # arguments for detectMultiScale: image to be searched, scale factor, min neighbors, min size of face
-    # returns top left, bottom right corner of the face
-    face = cascadeClassifierFace.detectMultiScale(greyVideoFrame, 1.1, 6, minSize=(40, 40))
-
-    # draw a rectangle on the face
-    for (x, y, w, h) in face:
-        #arguments for rectangle: frame to draw on, start pt, end pt, colour, thickness
-        cv2.rectangle(videoFrame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    return face
-
-def eyeDetection(videoFrame, cascadeClassifierEyes):
-    greyVideoFrame = cv2.cvtColor(videoFrame, cv2.COLOR_BGR2GRAY)
-    eyes = cascadeClassifierEyes.detectMultiScale(greyVideoFrame, scaleFactor=1.1, minNeighbors=6, minSize=(30, 30))
-    for (x, y, w, h) in eyes:
-        cv2.rectangle(videoFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    return eyes
