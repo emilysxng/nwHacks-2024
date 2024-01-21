23bac1e44ba327866264d056869b53fa1c785b20
tczg <tczg2004@gmail.com>
2024-01-21 10:46:55 -0800

    added stats

--- frontend/src/StudyModal.tsx
+++ frontend/src/StudyModal.tsx
@@ -20,6 +20,8 @@ import {
     CircularProgress,
     CircularProgressLabel,
     HStack,
+    VStack,
+    StackDivider,
   } from '@chakra-ui/react'
 import { useEffect, useRef, useState } from 'react'
 import WebcamWrapper, { WebcamWrapperMethods } from './WebcamWrapper'
@@ -43,22 +45,26 @@ function StudyModal() {
     
     const [goalInput, setGoalInput] = useState('')
     const [sessionInput, setSessionInput] = useState('')
-    const [shortBreakInput, setShortBreakInput] = useState('')
-    const [longBreakInput, setLongBreakInput] = useState('')
+    const [BreakInput, setShortBreakInput] = useState('')
     const [amountInput, setAmountInput] = useState('')
     const [, setButtonClicked] = useState(false); 
     const [studyState, setStudyState] = useState('idle');
 
+    const [studyProportion, setStudyProportion] = useState<number>()
+    const [sittingLongestLength, setSittingLongestLength] = useState<number>()
+    const [lookDownProportion, setLookDownProportion] = useState<number>()
+    const [afkProportion, setAfkProportion] = useState<number>()
+    const [afkLongestLength, setAfkLongestLength] = useState<number>()
+    const [afkCount, setAfkCount] = useState<number>()
+
     const handleGoalInputChange = (e: any) => setGoalInput(e.target.value)
     const handleSessionInputChange = (e : any) => setSessionInput(e.target.value)
-    const handleShortBreakInputChange = (e: any) => setShortBreakInput(e.target.value)
-    const handleLongBreakInputChange = (e: any) => setLongBreakInput(e.target.value)
+    const handleBreakInputChange = (e: any) => setShortBreakInput(e.target.value)
     const handleAmountInputChange = (e: any) => setAmountInput(e.target.value)
 
     const isGoalError = goalInput === ''
     const isSessionError = sessionInput === ''
-    const isShortBreakError = shortBreakInput === ''
-    const isLongBreakError = longBreakInput === ''
+    const isBreakError = BreakInput === ''
     const isAmountError = amountInput === ''
 
     const time = 60*(sessionInput as unknown as number);
@@ -73,7 +79,6 @@ function StudyModal() {
             setGoalInput('');
             setSessionInput('');
             setShortBreakInput('');
-            setLongBreakInput('');
             setAmountInput('');
             setStudyState('statistics');
         }
@@ -98,6 +103,12 @@ function StudyModal() {
 
     useEffect(() => {
         socket.on('study_session_end', (statistics: Statistics) => {
+            setStudyProportion((Number(statistics.studyProportion.toFixed(4)))*100);
+            setSittingLongestLength((Number(statistics.sittingLongestLength.toFixed(4))));
+            setLookDownProportion((Number(statistics.lookDownProportion.toFixed(4)))*100);
+            setAfkProportion((Number(statistics.afkProportion.toFixed(4)))*100);
+            setAfkLongestLength((Number((statistics.afkLongestLength).toFixed(4))));
+            setAfkCount(statistics.afkCount);
             console.log(statistics);
         });
     }, []);
@@ -130,14 +141,36 @@ function StudyModal() {
       </Box>)}
 
       {studyState === 'statistics' && (
-        <Box>
+        <VStack
+        divider={<StackDivider borderColor='gray.200' />}
+        spacing={4}
+        align='stretch'
+        >
             <Center>
-                <Text fontSize='3xl'>Stats</Text>
+                <Text fontSize='3xl'>Study Stats:</Text>
             </Center>
             <Center>
-                <Button onClick={onOpen} size='lg' mt="10px">Start Studying</Button>
+                <Text fontSize='lg'>Study Proportion: {studyProportion}%</Text>
             </Center>
-        </Box>
+            <Center>
+                <Text fontSize='lg'>Sitting Longest Length: {sittingLongestLength} secs</Text>
+            </Center>
+            <Center>
+                <Text fontSize='lg'>Look Down Proportion: {lookDownProportion}%</Text>
+            </Center>
+            <Center>
+                <Text fontSize='lg'>AFK Proportion: {afkProportion}%</Text>
+            </Center>
+            <Center>
+                <Text fontSize='lg'>AFK Longest Length: {afkLongestLength} secs</Text>
+            </Center>
+            <Center>
+                <Text fontSize='lg'>AFK Count: {afkCount} times</Text>
+            </Center>
+            <Center>
+                <Button onClick={onOpen} size='lg' mt="10px">Start Studying Again!</Button>
+            </Center>
+        </VStack>
       )}
 
       <Modal isOpen={isOpen} onClose={onClose}>
@@ -168,26 +201,15 @@ function StudyModal() {
                     <FormErrorMessage>Time for study session is required.</FormErrorMessage>
                 )}
             </FormControl>
-            <FormControl isInvalid={isShortBreakError}>
-                <FormLabel>Short Break time:</FormLabel>
-                <Input type='number' value={shortBreakInput} onChange={handleShortBreakInputChange} />
-                {!isShortBreakError ? (
+            <FormControl isInvalid={isBreakError}>
+                <FormLabel>Break time:</FormLabel>
+                <Input type='number' value={BreakInput} onChange={handleBreakInputChange} />
+                {!isBreakError ? (
                     <FormHelperText>
-                    Enter the time for your short break.
+                    Enter the time for your  break.
                     </FormHelperText>
                 ) : (
-                    <FormErrorMessage>Short break time is required.</FormErrorMessage>
-                )}
-            </FormControl>
-            <FormControl isInvalid={isLongBreakError}>
-                <FormLabel>Long Break time:</FormLabel>
-                <Input type='number' value={longBreakInput} onChange={handleLongBreakInputChange} />
-                {!isLongBreakError ? (
-                    <FormHelperText>
-                    Enter the time for your long break.
-                    </FormHelperText>
-                ) : (
-                    <FormErrorMessage>Long break time is required.</FormErrorMessage>
+                    <FormErrorMessage>Break time is required.</FormErrorMessage>
                 )}
             </FormControl>
             <FormControl isInvalid={isAmountError}>
@@ -205,7 +227,7 @@ function StudyModal() {
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={handleButtonClick} isDisabled={isGoalError || isAmountError || isLongBreakError || isShortBreakError || isSessionError}>
+            <Button colorScheme='blue' mr={3} onClick={handleButtonClick} isDisabled={isGoalError || isAmountError || isBreakError || isSessionError}>
               Start Studying!
             </Button>
           </ModalFooter>
